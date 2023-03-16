2444e5c9034cae4486bab34c8f0684b0e3624316
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2023-03-16 13:35:11 +1300

    Add Tree component

---
+++ _components/Tree.jsx
@@ -0,0 +1,46 @@
+const tree = (str, placeholder = ">") => {
+	const lines = str.split('\n');
+
+  for (let l = 0; l < lines.length; l++) {
+		const line = lines[l].split('');
+		for (let c = 0; c < line.length; c++) {
+			if (line[c] !== placeholder) continue;
+			
+			const lineAbove = lines[l-1]?.[c] === placeholder;
+			const lineBelow = lines[l+1]?.[c] === placeholder;
+			const lineLeft= lines[l][c-1] === placeholder;
+			const lineRight= lines[l][c+1] === placeholder;
+
+			if (lineLeft) {
+				line[c] = "─";
+			} else if (lineBelow && lineRight) {
+				line[c] = "├";
+			} else if (lineBelow) {
+				line[c] = "│";
+			} else {
+				line[c] = "└";
+			}
+		}
+		lines[l] = line.join('');
+	}
+
+	return lines.join('\n');
+};
+
+export default function ({ comp, children }) {
+  const code_str = children?.props?.children?.props?.children;
+  const tree_str = tree(code_str);
+  return (
+    <div className="c-code-block">
+      <div className="c-code-block__code">
+        <figure className="highlight">
+          <pre>
+            <code className={`language-plaintext`}>
+              {tree_str}
+            </code>
+          </pre>
+        </figure>
+      </div>
+    </div>
+  )
+}
