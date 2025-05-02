170d299d2630cf4323c66c225480a052dede45cb
Ella <ella@cloudcannon.com>
2025-05-02 02:18:03 +0000

    Updated 2 files via CloudCannon

--- cloudcannon.config.cjs
+++ cloudcannon.config.cjs
@@ -554,12 +554,6 @@ module.exports = {
         },
       },
     },
-    author_notes: {
-      type: "object",
-      options: {
-        subtype: "object",
-      },
-    },
     docshots_status: {
       type: "select",
       comment: "Select whether this article needs Docshots.",
