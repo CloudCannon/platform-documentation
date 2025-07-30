8fbcca4a074a8438bc6e79707ebdf8442cb36cec
Adon <adon@cloudcannon.com>
2025-07-30 14:54:20 +1200

    ...

--- .cloudcannon/copy-docshot-source.js
+++ .cloudcannon/copy-docshot-source.js
@@ -6,7 +6,7 @@ try {
   console.log("writing new config...")
   const source = yaml.load(fs.readFileSync("_data/docshots.yml", 'utf8'));
   let config = yaml.load(fs.readFileSync("cloudcannon.config.yml", 'utf8'));
-  config._snippets.docshot.preview.gallery.image[0].template = config._snippets.docshot.preview.gallery.image[0].template.replaceAll("{source}",source.source)
+  config._snippets.docshot.preview.gallery.image[0].template = `'https://cc-screenshots.imgix.net/${source.source}/{docshot_key}.webp'`
   fs.writeFileSync("cloudcannon.config.yml",yaml.dump(config));
   console.log("done writing new config...")
 } catch (e) {
