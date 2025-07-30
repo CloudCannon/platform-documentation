0d85956a90e8693b32965b7e5c7cc6a158eed5a4
Adon <adon@cloudcannon.com>
2025-07-30 14:22:04 +1200

    ...

---
+++ .cloudcannon/copy-docshot-source.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const yaml = require('js-yaml');
+
+// Get document, or throw exception on error
+try {
+  const source = yaml.load(fs.readFileSync("_data/docshots.yml", 'utf8'));
+  let config = yaml.load(fs.readFileSync("cloudcannon.config.yml", 'utf8'));
+  config._snippets.docshot.preview.gallery.image[0].template = config._snippets.docshot.preview.gallery.image[0].template.replaceAll("{source}",source.source)
+  fs.writeFileSync("cloudcannon.config.yml",yaml.dump(config));
+} catch (e) {
+  console.log(e);
+}
