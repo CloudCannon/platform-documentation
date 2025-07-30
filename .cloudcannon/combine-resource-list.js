c697069744492500d8c1c7acac12a4fa1e4e230b
Adon <adon@cloudcannon.com>
2025-07-31 08:38:40 +1200

    ...

--- .cloudcannon/combine-resource-list.js
+++ .cloudcannon/combine-resource-list.js
@@ -1,13 +1,25 @@
-const fs = require("fs");
+const fs = require('fs');
+const fg = require('fast-glob');
 const yaml = require('js-yaml');
+const matter = require('gray-matter');
 
 // Get document, or throw exception on error
+console.log("combining resources...")
+const all_resources = []
 try {
-  console.log("combining resources...")
-  
-  let config = yaml.load(fs.readFileSync("cloudcannon.config.yml", 'utf8'));
-  fs.writeFileSync("cloudcannon.config.yml",yaml.dump(config, {lineWidth:-1, forceQuotes:true}));
-  console.log("done writing new config...")
-} catch (e) {
-  console.log(e);
+  const entries = fg.globSync(["developer/**/*.mdx","user/**/*.mdx"])
+  for(const entry of entries)
+  {
+    const data = matter(fs.readFileSync(entry));
+    const obj = {
+      path:entry,
+      title:data.data.title,
+      _uuid:data.data._uuid
+    }
+    all_resources.push(obj)
+  }
+
+  fs.writeFileSync("_data/combined_resource_list.yml",yaml.dump(all_resources));
+} catch (err) {
+    console.error('Error reading directory:', err);
 }
