269d018022bde519f79143ed355cd4cb69a0eb40
Adon <adon@cloudcannon.com>
2025-08-06 11:47:13 +1200

    ...

--- assets/js/site.js
+++ assets/js/site.js
@@ -71,12 +71,193 @@ search.add(
     defaultMessage: "Search",
   })
 );
+
+// copied from pagefind core and modified
+class FilterPillsCustom {
+    constructor(opts = {}) {
+        this.instance = null;
+        this.wrapper = null;
+        this.pillContainer = null;
+        this.available = {};
+        this.selected = ["All"];
+        this.total = 0;
+        this.filterMemo = "";
+
+        this.filter = opts.filter;
+        this.ordering = opts.ordering ?? null;
+        this.alwaysShow = opts.alwaysShow ?? false;
+        this.selectMultiple = opts.selectMultiple ?? false;
+
+        if (!this.filter?.length) {
+            console.error(`[Pagefind FilterPills component]: No filter option supplied, nothing to display`);
+            return;
+        }
+
+        if (opts.containerElement) {
+            this.initContainer(opts.containerElement);
+        } else {
+            console.error(`[Pagefind FilterPills component]: No selector supplied for containerElement`);
+            return;
+        }
+    }
+
+    initContainer(selector) {
+        const container = document.querySelector(selector);
+        if (!container) {
+            console.error(`[Pagefind FilterPills component]: No container found for ${selector} selector`);
+            return;
+        }
+        container.innerHTML = "";
+        const id = `pagefind_modular_filter_pills_${this.filter}`;
+
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("pagefind-modular-filter-pills-wrapper")
+        wrapper.setAttribute("role","group")
+        wrapper.setAttribute("aria-labelledby",id)
+
+        if (!this.alwaysShow) {
+            wrapper.setAttribute("data-pfmod-hidden",true)
+        }
+
+        let heading = document.createElement("h3")
+        heading.innerText = "Category"
+        wrapper.append(heading)
+        
+        let div = document.createElement("div")
+        div.id = id
+        div.classList.add("pagefind-modular-filter-pills-label")
+        div.setAttribute("data-pfmod-sr-hidden",true)
+        div.innerText = `Filter results by ${this.filter}`
+        
+        wrapper.append(div)
+
+        this.pillContainer = document.createElement("div")
+        this.pillContainer.classList.add("pagefind-modular-filter-pills")
+        wrapper.append(this.pillContainer)
+
+        this.wrapper = wrapper
+        container.append(wrapper)
+    }
+
+    update() {
+        const filterMemo = this.available.map(a => a[0]).join("~");
+        if (filterMemo == this.filterMemo) {
+            this.updateExisting();
+        } else {
+            this.renderNew();
+            this.filterMemo = filterMemo;
+        }
+    }
+
+    pushFilters() {
+        const selected = this.selected.filter(v => v !== "All");
+        this.instance.triggerFilter(this.filter, selected);
+    }
+
+    pillInner(val, count) {
+        if (this.total) {
+            return `<span aria-label="${val}"><span class="filter_val">${val}</span><span class="filter_count">${count}</span></span>`;
+        } else {
+            return `<span aria-label="${val}">${val}</span>`;
+        }
+    }
+
+    renderNew() {
+        this.available.forEach(([val, count]) => {
+          let button = document.createElement("button")
+          button.classList.add("pagefind-modular-filter-pill")
+          button.innerHTML = this.pillInner(val, count)
+          button.setAttribute("aria-pressed",this.selected.includes(val))
+          button.setAttribute("type","button")
+
+          button.addEventListener("click", () => {
+            if (val === "All") {
+                this.selected = ["All"];
+            } else if (this.selected.includes(val)) {
+                this.selected = this.selected.filter(v => v !== val);
+            } else if (this.selectMultiple) {
+                this.selected.push(val);
+            } else {
+                this.selected = [val];
+            }
+            if (!this.selected?.length) {
+                this.selected = ["All"];
+            } else if (this.selected?.length > 1) {
+                this.selected = this.selected.filter(v => v !== "All");
+            }
+            this.update();
+            this.pushFilters();
+        })
+        this.pillContainer.append(button)
+      });
+    }
+
+    updateExisting() {
+        const pills = [...this.pillContainer.childNodes];
+        this.available.forEach(([val, count], i) => {
+            pills[i].innerHTML = this.pillInner(val, count);
+            pills[i].setAttribute("aria-pressed", this.selected.includes(val));
+        });
+    }
+
+    register(instance) {
+        this.instance = instance;
+        this.instance.on("filters", (filters) => {
+            if (!this.pillContainer) return;
+
+            if (this.selectMultiple) {
+                filters = filters.available;
+            } else {
+                filters = filters.total;
+            }
+
+            let newlyAvailable = filters[this.filter];
+            if (!newlyAvailable) {
+                console.warn(`[Pagefind FilterPills component]: No possible values found for the ${this.filter} filter`);
+                return;
+            }
+            this.available = Object.entries(newlyAvailable);
+
+            if (Array.isArray(this.ordering)) {
+                this.available.sort((a, b) => {
+                    const apos = this.ordering.indexOf(a[0]);
+                    const bpos = this.ordering.indexOf(b[0]);
+                    return (apos === -1 ? Infinity : apos) - (bpos === -1 ? Infinity : bpos);
+                });
+            } else {
+                this.available.sort((a, b) => {
+                    return a[0].localeCompare(b[0]);
+                });
+            }
+            this.available.unshift(["All", this.total]);
+            this.update();
+        });
+
+        instance.on("results", (results) => {
+            if (!this.pillContainer) return;
+            this.total = results?.unfilteredResultCount || 0;
+
+            if (this.available?.[0]?.[0] === "All") {
+                this.available[0][1] = this.total;
+            }
+
+            if (this.total || this.alwaysShow) {
+                this.wrapper.removeAttribute("data-pfmod-hidden");
+            } else {
+                this.wrapper.setAttribute("data-pfmod-hidden", "true");
+            }
+            this.update();
+        });
+    }
+}
+
 search.add(
-  new FilterPills({
+  new FilterPillsCustom({
     containerElement: "#searchfilter",
     filter: "site",
   })
 );
+
 
 let recentSearches = null,
   thisSearch = null;
@@ -116,6 +297,13 @@ if (messageElement) {
     }
   });
 }
+
+search.on("results", (results) => {
+    if(results.results.length > 0)
+      document.getElementById("searchcontainer").classList.add("has-results")
+    else
+      document.getElementById("searchcontainer").classList.remove("has-results")
+});
 
 // Prefetch links
 const anchorTagElements = document.getElementsByTagName("a");
