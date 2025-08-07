9e1e88935fb667bdff478e6d5593a9472205c910
Adon <adon@cloudcannon.com>
2025-08-07 14:41:16 +1200

    ...

--- .cloudcannon/combine-resource-list.js
+++ .cloudcannon/combine-resource-list.js
@@ -22,6 +22,23 @@ try {
   }
 
   fs.writeFileSync("_data/combined_resource_list.yml",yaml.dump(all_resources));
+
+  const all_guides = []
+  const guides = fg.globSync(["developer/guides/**/*.yml","user/guides/**/*.yml"])
+  for(const guide of guides)
+  {
+    const data = yaml.load(fs.readFileSync(guide, 'utf-8'));
+    const obj = {
+      path:guide,
+      guide_id: data.guide_id,
+      guide_title: data.guide_title,
+      guide_summary:data.guide_summary,
+      guide_image:data.guide_image
+    }
+    all_guides.push(obj)
+  }
+
+  fs.writeFileSync("_data/combined_guides.yml",yaml.dump(all_guides));
 } catch (err) {
     console.error('Error reading directory:', err);
 }
