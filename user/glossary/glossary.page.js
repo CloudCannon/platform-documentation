a59bdd6ecda4d0c99fa63cc2b1072dcbeb3b3e33
Adon <adon@cloudcannon.com>
2025-08-20 12:14:43 +1200

    ...

--- user/glossary/glossary.page.js
+++ user/glossary/glossary.page.js
@@ -1,9 +1,21 @@
 export default function* ({ search }) {
-  for(const letter of [...Array(26).keys()].map((n) => String.fromCharCode(97 + n)))
-  {
-    yield{
-        url: `/user-glossary/${letter}/`,
-        layout: "layouts/glossary-list.njk"
-    };
+  for (const letter of [...Array(26).keys()].map((n) => String.fromCharCode(97 + n))) {
+    try {
+      const info = Deno.statSync(`user/glossary/${letter}`);
+      if (info.isDirectory) {
+        yield {
+          url: `/user-glossary/${letter}/`,
+          layout: "layouts/glossary-list.njk"
+        };
+      } else {
+        console.log("Path exists but is not a directory");
+      }
+    } catch (err) {
+      if (err instanceof Deno.errors.NotFound) {
+        console.log("Directory does not exist");
+      } else {
+        throw err;
+      }
+    }
   }
 }
