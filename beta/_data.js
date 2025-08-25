327f02fb965f4d335029482c562a8d8eab87cba1
Ella <ella@cloudcannon.com>
2025-08-25 21:37:23 +0000

    Add data file to beta collection

---
+++ beta/_data.js
@@ -0,0 +1,8 @@
+export const layout = "layouts/article.njk"
+export const date = "git last modified"
+export function url(page) {
+  let u = page.data.url
+  u = u.split("/").slice(1)
+  u = `${u[0]}-${u[1]}/${u[2]}`
+  return `/${u}/`;
+}
