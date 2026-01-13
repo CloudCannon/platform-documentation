edfc02a9ee2efc4616dfb224b4f01d9a4a3cbc52
Adon <adon@cloudcannon.com>
2026-01-13 13:02:48 +1300

    ...

--- _components/GlossaryTerm.jsx
+++ _components/GlossaryTerm.jsx
@@ -1,7 +1,6 @@
 
 export default function ({comp, term, children}, helpers) {
-  const content = `<h3>children</h3>${helpers.get_glossary_term(term)}`;
-
+  const content = `<h3>${children.__html}</h3>${helpers.get_glossary_term(term)}`;
   return (
     <span
       x-data="{ tooltipopen: false,
