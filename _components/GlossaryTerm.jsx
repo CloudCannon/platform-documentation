4ed2a29e21ffe1769d0b34472a6b8343ee7137a0
Adon <adon@cloudcannon.com>
2026-01-28 21:46:40 +1300

    Better styling on the glossary term snippets

--- _components/GlossaryTerm.jsx
+++ _components/GlossaryTerm.jsx
@@ -1,19 +1,42 @@
 
 export default function ({comp, term, children}, helpers) {
-  const content = `<h3>${children.__html}</h3>${helpers.get_glossary_term(term)}`;
+  const content = `<span class="eyebrow">Glossary term</span><h3>${children.__html}</h3>${helpers.get_glossary_term(term)}`;
   return (
     <span
-      x-data="{ tooltipopen: false,
-        update() {
-          const rect = this.$el.getBoundingClientRect();
-          this.$refs.tooltip.style.top =
+      x-data="{
+        tooltipopen: false,
+        closeTimeout: null,
+
+        open: function () {
+          this.cancelClose();
+          this.tooltipopen = true;
+          $nextTick(() => this.update())
+        },
+
+        scheduleClose: function () {
+          this.cancelClose();
+          this.closeTimeout = setTimeout(() => {
+            this.tooltipopen = false;
+          }, 100)
+        },
+
+        cancelClose: function () {
+          if (this.closeTimeout) {
+            clearTimeout(this.closeTimeout);
+            this.closeTimeout = null;
+          }
+        },
+
+        update: function () {
+          const rect = $el.getBoundingClientRect();
+          $refs.tooltip.style.top =
             rect.bottom + window.scrollY + 6 + 'px';
-          this.$refs.tooltip.style.left =
+          $refs.tooltip.style.left =
             rect.left + window.scrollX + 'px';
-        } 
+        }
       }"
-      x-on:mouseover="tooltipopen = true; $nextTick(() => update())"
-      x-on:mouseout="tooltipopen = false"
+      x-on:mouseenter="open"
+      x-on:mouseleave="scheduleClose"
       class="glossary-term-highlight"
     >
       {children}
@@ -22,14 +45,14 @@ export default function ({comp, term, children}, helpers) {
         <div
           x-show="tooltipopen"
           x-transition
-          class="term-definition"
           x-ref="tooltip"
+          class="term-definition"
+          x-on:mouseenter="cancelClose"
+          x-on:mouseleave="scheduleClose"
         >
           <div dangerouslySetInnerHTML={{ __html: content }} />
         </div>
       </template>
     </span>
   );
-  //const content_md = helpers.get_md(content);
-  //const glossary = site.pages.filter((p) => p.data.collection === "glossary");
 }
