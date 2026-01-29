2c7bbbdf455e5c4272b96bdba159e1b907354104
George Phillips <george@cloudcannon.com>
2026-01-29 16:45:40 +1300

    fix typescript import again

--- .cloudcannon/download-permissions.js
+++ .cloudcannon/download-permissions.js
@@ -1,4 +1,4 @@
-import process from "node:process";
+const process = require("node:process");
 const fs = require("fs");
 const filepath = "_data/permissions.json";
 const treeUrl = "https://app.cloudcannon.com/permissions-tree";
